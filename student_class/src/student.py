d68f659a5cdf1572aa24dbfd8785a20f3ec8b27a
Sara <sluisdejesus@gmail.com>
2021-06-21 22:17:25 +0100

    initial commit

---
+++ student_class/src/student.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, input_name, input_cohort):
+        self.name = input_name
+        self.cohort = input_cohort
+        
+        def student_has_name(self):
+            return input_name()
+
+        def student_has_cohort(self):
+            return input_cohort
+
+        
---
+++ team_class/src/team.py
@@ -0,0 +1,28 @@
+class Team:
+    def __init__(self, input_name, input_players,input_coach):
+        self.name = input_name
+        self.players = input_players
+        self.coach = input_coach
+        self.add_player =
+        self.has_player = 
+        self.has_player = 
+
+    def team_has_name(self):
+        return self.name
+
+    def team_has_players(self):
+        return self.players
+
+    def team_has_coach(self):
+        return self.coach
+
+    def can_add_new_player_to_team(self):
+        self.coach = "John Candy"
+
+    # def can_add_new_player_to_team(self):
+
+    def check_player_in_team(self):
+        find_player = False
+        for player in self.players:
+           if player == find_player:
+              find_player = True
